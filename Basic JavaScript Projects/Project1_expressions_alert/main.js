17654e3806db9566ae1c194483e968820d4040df
Dead-Raddish <107513762+Dead-Raddish@users.noreply.github.com>
2022-08-23 13:03:55 -0700

    Basic hmtl page created, js file created, var assigned, added expression, concatenated and  linked to html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,5 @@
+var Sent1 = "This is the beginning of the string", Sent2 = "and this is the end of the string"; //Assigning variables
+
+var CompleteSent = Sent1 + Sent2; //Concatenating variables
+
+document.write(CompleteSent); //Printing varibles
